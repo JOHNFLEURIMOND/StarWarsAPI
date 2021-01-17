2d315602f4051503b469abe8aa269d91e6f12646
johnfleurimond <johncfleurimond@gmail.com>
2021-01-17 15:51:31 -0500

    third commit: update read.me

---
+++ assets/src/index.ts
@@ -0,0 +1,12 @@
+import { Header } from "./Header";
+import { Card } from "./Card";
+import { CharacterPage } from "./CharacterPage";
+import { Hero } from "./Hero";
+
+
+export {
+  Header,
+  Card,
+  CharacterPage,
+  Hero
+};
